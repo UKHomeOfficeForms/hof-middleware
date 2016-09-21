af30a12fd022d3d32207b94fe4e94b387bc58785
joefitter <hello@joefitter.com>
2016-09-21 11:19:19 +0100

    enhancement - allow deep translate to work with multiple value fields
    * cast value from sessionModel to string
    * look up comma separated string reoresentation of array

--- test/lib/deep-translate.js
+++ test/lib/deep-translate.js
@@ -41,6 +41,19 @@ describe('deepTranslate middleware', () => {
           },
           'default': 'This is another default label'
         }
+      },
+      'multi-value': {
+        'header': {
+          'multi-value-field': {
+            'value-1': '1',
+            'value-1,value-2': '1 and 2',
+            'value-1,value-3': '1 and 3',
+            'value-1,value-2,value-3': '1, 2 and 3',
+            'value-2': '2',
+            'value-2,value-3': '2 and 3',
+            'value-3': '3'
+          }
+        }
       }
     };
     next = sinon.stub();
@@ -86,5 +99,55 @@ describe('deepTranslate middleware', () => {
     req.sessionModel.get.withArgs('dependent-field-1').returns('correct-value');
     req.sessionModel.get.withArgs('dependent-field-2').returns('correct-value');
     req.translate('another-field.header').should.be.equal('This should be looked up');
+  });
+
+  describe('Multi value fields', () => {
+    it('returns 1, 2 and 3 if field has all 3 values', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns([
+        'value-1',
+        'value-2',
+        'value-3'
+      ]);
+      req.translate('multi-value.header').should.be.equal('1, 2 and 3');
+    });
+
+    it('returns 1 and 2 if field has value-1 and value-2', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns([
+        'value-1',
+        'value-2'
+      ]);
+      req.translate('multi-value.header').should.be.equal('1 and 2');
+    });
+
+    it('returns 1 and 3 if field has value-1 and value-3', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns([
+        'value-1',
+        'value-3'
+      ]);
+      req.translate('multi-value.header').should.be.equal('1 and 3');
+    });
+
+    it('returns 1 if field has value-1', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns('value-1');
+      req.translate('multi-value.header').should.be.equal('1');
+    });
+
+    it('returns 2 if field has value-2', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns('value-2');
+      req.translate('multi-value.header').should.be.equal('2');
+    });
+
+    it('returns 2 and 3 if field has value-2 and value-3', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns([
+        'value-2',
+        'value-3'
+      ]);
+      req.translate('multi-value.header').should.be.equal('2 and 3');
+    });
+
+    it('returns 3 if field has value-3', () => {
+      req.sessionModel.get.withArgs('multi-value-field').returns('value-3');
+      req.translate('multi-value.header').should.be.equal('3');
+    });
   });
 });
